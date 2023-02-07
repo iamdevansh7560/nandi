31534ffe698ae31d635c9508fc7ced16cac2e95c
796220 <devansh.sharma1920@gmail.com>
2023-02-07 19:47:20 +0530

    f

--- controllers/twilio.controller.js
+++ controllers/twilio.controller.js
@@ -12,27 +12,21 @@
 // }
 
 const sendMessage = async (req, res) => {
+
     try {
-        const client = require('twilio')('ACf90d13540049e308163357c6ba5490d6', '27c7c52d8878df3af81645af85654d6f', {
-            lazyLoading: true
-        });
-        console.log(req.body);
+        const accountSid = 'AC4d5b0283e73f2592e338161d305cefd7'; 
+        const authToken = '7a3f8e80bac8b24190859471d9c1a2d7'; 
+        const client = require('twilio')(accountSid, authToken); 
         
-        
-        // let message = req.Body.body;
-        // let senderID = req.body.From;
-        // console.log(message);
-        // console.log(senderID);
-
-
-        await client.messages
-            .create({
-                body: 'This is the ship that made the Kessel Run in fourteen parsecs?',
-                from: '+16513723314',
-                to: '+919796617824'
-            })
-            .then(res.send("ok"))
-
+         
+        client.messages 
+              .create({ 
+                 body: 'Hello! This is an editable text message. You are free to change it and write whatever you like.', 
+                 from: 'whatsapp:+14155238886',       
+                 to: 'whatsapp:+919796617824' 
+               }) 
+              .then(message => res.json("ok")) 
+              .done();
 
     } catch (error) {
         console.log(`Error at sendMessage -->${error}`);
